132f4fe959277867a98de22d38e3814e379832cf
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2023-09-09 19:09:27 +0200

    Add basic layout to print API information

--- src/weather-data.js
+++ src/weather-data.js
@@ -5,8 +5,30 @@ async function getWeatherData(location) {
     try {
         let weather = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=4626ed69a3ed4fd284a194520230509&q=${location}&days=3`); 
         let weatherData = await weather.json();
+        
+        let city = document.querySelector("#city");
+        city.textContent = weatherData.location.name
+
+        let date = document.querySelector("#date");
+        date.textContent = weatherData.location.localtime;
+
+        let currentWeather = document.querySelector("#currentWeather span");
+        currentWeather.textContent = weatherData.current.condition.text;
+        
+        let feelsLike = document.querySelector("#temperatureFelt span");
+        feelsLike.textContent = weatherData.current.feelslike_c;
+
+        let humidity = document.querySelector("#humidity span");
+        humidity.textContent = weatherData.current.humidity;
+
+        let rainChance = document.querySelector("#rainChance span");
+        rainChance.textContent = weatherData.forecast.forecastday[0].daily_chance_of_rain;
+
+        let windSpeed = document.querySelector("#windSpeed span");
+        windSpeed.textContent = weatherData.current.wind_kph;
+
         console.log(weatherData);
-    }catch {
+    } catch {
         alert("Whoops, something went wrong");
     }
 
