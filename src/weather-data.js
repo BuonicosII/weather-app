5533217c5a70d49647c423730fb74f59be85e7e5
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2023-09-13 21:37:09 +0200

    Add background image on weather condition change

--- src/weather-data.js
+++ src/weather-data.js
@@ -6,7 +6,7 @@ async function getWeatherData(location) {
         let weather = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=4626ed69a3ed4fd284a194520230509&q=${location}&days=3`); 
         let weatherData = await weather.json();
         
-        let city = document.querySelector("#city span");
+        let city = document.querySelector("#city div");
         city.textContent = weatherData.location.name
 
         let country = document.querySelector("#country span");
@@ -26,7 +26,71 @@ async function getWeatherData(location) {
         let currentTemperatureMetric = document.querySelector("#currentTemperature .metric span");
         currentTemperatureMetric.textContent = weatherData.current.temp_c + " " + "°C";
 
-        let currentWeatherIcon = document.querySelector("#currentWeather img");
+        let background= document.querySelector("body");
+
+        switch (weatherData.current.condition.code) {
+            case 1000: 
+                if (weatherData.current.is_day === 1) {
+                    background.setAttribute("style", "background-image: url(../src/Background/sun.jpg); background-size: cover;");
+                } else if (weatherData.current.is_day === 0) {
+                    background.setAttribute("style", "background-image: url(../src/Background/clearnight.jpg); background-size: cover;");
+                }
+                break;
+            case 1003:
+            case 1006:
+            case 1009:
+                background.setAttribute("style", "background-image: url(../src/Background/cloudy.jpg); background-size: cover;");
+                break;
+            case 1030:
+            case 1135:
+            case 1147:
+                background.setAttribute("style", "background-image: url(../src/Background/fog.jpg); background-size: cover;");
+                break;
+            case 1063:
+            case 1087:
+            case 1150:
+            case 1153:
+            case 1168:
+            case 1171:
+            case 1180:
+            case 1183:
+            case 1186:
+            case 1189:
+            case 1192:
+            case 1195:
+            case 1198:
+            case 1201:
+            case 1240:
+            case 1243:
+            case 1246:
+            case 1273:
+            case 1276:
+                background.setAttribute("style", "background-image: url(../src/Background/rain.jpg); background-size: cover;");
+                break;
+            case 1066:
+            case 1069:
+            case 1073:
+            case 1114:
+            case 1117:
+            case 1204:
+            case 1207:
+            case 1210:
+            case 1213:
+            case 1216:
+            case 1219:
+            case 1222:
+            case 1225:
+            case 1237:
+            case 1249:
+            case 1252:
+            case 1255:
+            case 1258:
+            case 1261:
+            case 1264:
+            case 1279:
+            case 1282:
+                background.setAttribute("style", "background-image: url(Background/snow.jpg); background-size: cover;");
+        }
         //create switch case scenario for currentWeather Icon and app backgroud
 
 
