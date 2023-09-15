a927417da7fef41143d49f6463ce787e26584e78
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2023-09-15 23:50:24 +0200

    Add dropdown test

--- src/autocomplete.js
+++ src/autocomplete.js
@@ -1,10 +1,19 @@
 async function autocomplete () {
     let locationField = document.querySelector("#location");
+    let dropDownHolder = document.querySelector("#dropDownHolder");
     try {
         let locationsList = await fetch(`https://api.weatherapi.com/v1/search.json?key=4626ed69a3ed4fd284a194520230509&q=${locationField.value}`);
         let locationsListArray = await locationsList.json();
+        let dropDown = document.createElement("div");
+        dropDown.classList.add("drop-down");
+        dropDownHolder.appendChild(dropDown)
         for (const location of locationsListArray) {
-            console.log(location.name, location.country);
+            let suggestion = document.createElement("div");
+            suggestion.classList.add("suggestion");
+            let text = document.createElement("div");
+            text.textContent = location.name + " , " + location.country;
+            suggestion.appendChild(text)
+            dropDown.appendChild(suggestion);
         }
     } catch {
         console.log("Server is taking its time")
