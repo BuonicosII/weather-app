d985ec3d636b5961cd6a5016e5df3a882516a503
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2023-09-16 16:55:14 +0200

    Add functioning dropdown to the search input

--- src/autocomplete.js
+++ src/autocomplete.js
@@ -1,22 +1,41 @@
 async function autocomplete () {
-    let locationField = document.querySelector("#location");
-    let dropDownHolder = document.querySelector("#dropDownHolder");
     try {
+        //get the location data through API 
+        let locationField = document.querySelector("#location");
+
         let locationsList = await fetch(`https://api.weatherapi.com/v1/search.json?key=4626ed69a3ed4fd284a194520230509&q=${locationField.value}`);
         let locationsListArray = await locationsList.json();
+        
+        //in case the suggestion drop-down is already populated remove it 
+        let dropDownHolder = document.querySelector("#dropDownHolder");
+
+        while (dropDownHolder.hasChildNodes()) {
+            dropDownHolder.removeChild(dropDownHolder.firstChild)
+        }
+
+        if (locationsListArray.length > 0) {
+
+        //create a new updated suggestion drop-down
         let dropDown = document.createElement("div");
-        dropDown.classList.add("drop-down");
+        dropDown.setAttribute("id", "drop-down");
         dropDownHolder.appendChild(dropDown)
+
+        //create a div for each possible location and append it to the suggestion dropdown
         for (const location of locationsListArray) {
             let suggestion = document.createElement("div");
             suggestion.classList.add("suggestion");
             let text = document.createElement("div");
-            text.textContent = location.name + " , " + location.country;
+            text.textContent = location.name + ", " + location.country;
             suggestion.appendChild(text)
             dropDown.appendChild(suggestion);
         }
+
+        dropDown.lastChild.classList.remove("suggestion");
+        dropDown.lastChild.classList.add("lastSuggestion");
+        
+        }
     } catch {
-        console.log("Server is taking its time")
+        alert("Server is taking its time")
     }
 }
 
